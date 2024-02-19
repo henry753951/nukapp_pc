6520b97e45e813f6f551a06616101df4176c7bc0
henry <henryboy753951@gmail.com>
2024-02-19 17:29:50 +0800

    fix macos window effect

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -16,8 +16,7 @@ pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>
 
     // macOS
     #[cfg(target_os = "macos")]
-    apply_vibrancy(&window, NSVisualEffectMaterial::FullScreenUI)
-        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+    apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None).expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
     // Windows
     #[cfg(target_os = "windows")]
