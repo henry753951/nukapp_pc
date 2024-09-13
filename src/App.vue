98ed295c43ee406c85f59954fdb799b0ed62b618
henry753951 <henryboy753951@gmail.com>
2024-09-13 08:51:48 +0800

    chore: Update Sidebar.vue component formatting and user card display

--- src/App.vue
+++ src/App.vue
@@ -1,160 +1,160 @@
 <script lang="ts">
-  import { logger } from "./logger";
+import { logger } from "./logger";
 
-  import { useRoute } from "vue-router";
-  import { h, provide, ref } from "vue";
-  import { theme } from "ant-design-vue";
-  import { useThemeStore } from "./stores/theme";
-  import { useUserStore } from "./stores/user";
-  import variables from "./variables";
-  // Components
-  import window_control from "./components/window_control.vue";
-  import LoginModal from "./components/modals/LoginModal.vue";
-  import Sidebar from "./components/Sidebar.vue";
-  // tauri
-  import { appWindow } from "@tauri-apps/api/window";
-  // import { platform } from "@tauri-apps/plugin-os";
-  import { router } from "./router";
-  // const platformName = await platform();
-  export default {
-    components: {
-      window_control,
-      Sidebar,
-      LoginModal,
-    },
-    data() {
-      return {
-        layoutContent: null as HTMLDivElement | null,
-        debug: false,
-        themeData: {
-          algorithm: theme.defaultAlgorithm,
-          light_token: {
-            colorBgLayout: "#ffffff80",
-            colorPrimaryBg: "#00000015",
-            colorPrimary: "#808080",
-            colorSiderBg: "#ffffff05",
-          },
-          dark_token: {
-            colorBgLayout: "#00000080",
-            colorPrimaryBg: "#ffffff15",
-            colorPrimary: "#ffffff",
-            colorSiderBg: "#00000005",
-          },
-          token: {
-            colorBgLayout: "#ffffff80",
-            colorPrimaryBg: "#00000015",
-            colorPrimary: "#808080",
-            colorSiderBg: "#ffffff05",
-          },
+import { useRoute } from "vue-router";
+import { h, provide, ref } from "vue";
+import { theme } from "ant-design-vue";
+import { useThemeStore } from "./stores/theme";
+import { useUserStore } from "./stores/user";
+import variables from "./variables";
+// Components
+import window_control from "./components/window_control.vue";
+import LoginModal from "./components/modals/LoginModal.vue";
+import Sidebar from "./components/Sidebar.vue";
+// tauri
+import { appWindow } from "@tauri-apps/api/window";
+// import { platform } from "@tauri-apps/plugin-os";
+import { router } from "./router";
+// const platformName = await platform();
+export default {
+  components: {
+    window_control,
+    Sidebar,
+    LoginModal,
+  },
+  data() {
+    return {
+      layoutContent: null as HTMLDivElement | null,
+      debug: false,
+      themeData: {
+        algorithm: theme.defaultAlgorithm,
+        light_token: {
+          colorBgLayout: "#ffffff80",
+          colorPrimaryBg: "#00000015",
+          colorPrimary: "#808080",
+          colorSiderBg: "#ffffff05",
         },
-        mediaQueryList: null as MediaQueryList | null,
-        systemTheme: "",
-        currentTheme: "",
-        isFocused: true,
-        currentRoute: useRoute(),
-      };
-    },
-    provide() {
-      return {
-        scrollToTop: this.scrollToTop,
-      };
-    },
-    mounted() {
-      // logger.info(`Platform: ${platformName}`);
-      this.layoutContent = this.$refs.layoutContent as HTMLDivElement;
-      appWindow.listen("tauri://focus", ({}) => {
-        this.focused(true);
+        dark_token: {
+          colorBgLayout: "#00000080",
+          colorPrimaryBg: "#ffffff15",
+          colorPrimary: "#ffffff",
+          colorSiderBg: "#00000005",
+        },
+        token: {
+          colorBgLayout: "#ffffff80",
+          colorPrimaryBg: "#00000015",
+          colorPrimary: "#808080",
+          colorSiderBg: "#ffffff05",
+        },
+      },
+      mediaQueryList: null as MediaQueryList | null,
+      systemTheme: "",
+      currentTheme: "",
+      isFocused: true,
+      currentRoute: useRoute(),
+    };
+  },
+  provide() {
+    return {
+      scrollToTop: this.scrollToTop,
+    };
+  },
+  mounted() {
+    // logger.info(`Platform: ${platformName}`);
+    this.layoutContent = this.$refs.layoutContent as HTMLDivElement;
+    appWindow.listen("tauri://focus", ({}) => {
+      this.focused(true);
+    });
+    appWindow.listen("tauri://blur", ({}) => {
+      this.focused(false);
+    });
+    // System theme
+    this.mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+    this.systemTheme = this.mediaQueryList.matches ? "dark" : "light";
+    this.$nextTick(() => {
+      this.mediaQueryList!.addEventListener("change", (e) => {
+        this.systemTheme = e.matches ? "dark" : "light";
       });
-      appWindow.listen("tauri://blur", ({}) => {
-        this.focused(false);
-      });
-      // System theme
-      this.mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
-      this.systemTheme = this.mediaQueryList.matches ? "dark" : "light";
-      this.$nextTick(() => {
-        this.mediaQueryList!.addEventListener("change", (e) => {
-          this.systemTheme = e.matches ? "dark" : "light";
-        });
-        if (this.themeStore.theme === "default") {
-          this.switchTheme(this.systemTheme);
-        }
-      });
-      if (this.themeStore.theme) {
-        this.switchTheme(this.themeStore.theme);
+      if (this.themeStore.theme === "default") {
+        this.switchTheme(this.systemTheme);
       }
-      useUserStore().invokeLogin();
+    });
+    if (this.themeStore.theme) {
+      this.switchTheme(this.themeStore.theme);
+    }
+    useUserStore().invokeLogin();
+  },
+  unmounted() {
+    this.mediaQueryList!.removeEventListener("change", () => {});
+  },
+  computed: {
+    themeStore() {
+      return useThemeStore();
     },
-    unmounted() {
-      this.mediaQueryList!.removeEventListener("change", () => {});
+  },
+  methods: {
+    scrollToTop() {
+      logger.info("Scroll to top");
+      if (this.layoutContent !== null) {
+        this.layoutContent!.scrollTop = 0;
+      }
     },
-    computed: {
-      themeStore() {
-        return useThemeStore();
-      },
-    },
-    methods: {
-      scrollToTop() {
-        logger.info("Scroll to top");
-        if (this.layoutContent !== null) {
-          this.layoutContent!.scrollTop = 0;
-        }
-      },
-      focused(focus: boolean) {
-        this.isFocused = focus;
-        if (!focus) {
-          if (this.currentTheme === "dark") {
-            document.documentElement.style.setProperty(
-              "background-color",
-              "#000000"
-            );
-          } else {
-            document.documentElement.style.setProperty(
-              "background-color",
-              "#ffffff"
-            );
-          }
+    focused(focus: boolean) {
+      this.isFocused = focus;
+      if (!focus) {
+        if (this.currentTheme === "dark") {
+          document.documentElement.style.setProperty(
+            "background-color",
+            "#000000"
+          );
         } else {
-          document.documentElement.style.removeProperty("background-color");
+          document.documentElement.style.setProperty(
+            "background-color",
+            "#ffffff"
+          );
         }
-      },
-      switchTheme(mode: string) {
-        this.currentTheme = mode;
-        for (let key of variables) {
-          let value = `var(--${mode}-${key})`;
-          document.documentElement.style.setProperty(`--${key}`, value);
-        }
-        this.themeData = {
-          algorithm:
-            mode === "dark" ? theme.darkAlgorithm : theme.defaultAlgorithm,
-          token:
-            mode === "dark"
-              ? { ...this.themeData.dark_token }
-              : { ...this.themeData.light_token },
-          dark_token: {
-            ...this.themeData.dark_token,
-          },
-          light_token: {
-            ...this.themeData.light_token,
-          },
-        };
-      },
+      } else {
+        document.documentElement.style.removeProperty("background-color");
+      }
     },
+    switchTheme(mode: string) {
+      this.currentTheme = mode;
+      for (let key of variables) {
+        let value = `var(--${mode}-${key})`;
+        document.documentElement.style.setProperty(`--${key}`, value);
+      }
+      this.themeData = {
+        algorithm:
+          mode === "dark" ? theme.darkAlgorithm : theme.defaultAlgorithm,
+        token:
+          mode === "dark"
+            ? { ...this.themeData.dark_token }
+            : { ...this.themeData.light_token },
+        dark_token: {
+          ...this.themeData.dark_token,
+        },
+        light_token: {
+          ...this.themeData.light_token,
+        },
+      };
+    },
+  },
 
-    watch: {
-      "themeStore.theme"(val) {
-        if (val === "default") {
-          this.switchTheme(this.systemTheme);
-        } else {
-          this.switchTheme(val);
-        }
-      },
-      systemTheme(val) {
-        if (this.themeStore.theme === "default") {
-          this.switchTheme(val);
-        }
-      },
+  watch: {
+    "themeStore.theme"(val) {
+      if (val === "default") {
+        this.switchTheme(this.systemTheme);
+      } else {
+        this.switchTheme(val);
+      }
     },
-  };
+    systemTheme(val) {
+      if (this.themeStore.theme === "default") {
+        this.switchTheme(val);
+      }
+    },
+  },
+};
 </script>
 
 <template>
@@ -173,7 +173,8 @@
             <div
               class="flex justify-between px-2 shadow-sm"
               style="background-color: rgba(255, 255, 255, 0.06)"
-              data-tauri-drag-region>
+              data-tauri-drag-region
+            >
               <a-breadcrumb style="margin: 10px 0">
                 <a-breadcrumb-item>Home</a-breadcrumb-item>
                 <a-breadcrumb-item
@@ -188,10 +189,12 @@
               :style="{
                 margin: 0,
                 minHeight: '280px',
-              }">
+              }"
+            >
               <div
                 ref="layoutContent"
-                style="height: 100%; overflow-y: overlay; overflow-x: hidden">
+                style="height: 100%; overflow-y: overlay; overflow-x: hidden"
+              >
                 <router-view />
               </div>
             </a-layout-content>
@@ -223,21 +226,21 @@
 </template>
 
 <style scoped lang="scss">
-  .debug-view {
-    /* right bottom fixed */
-    position: fixed;
-    bottom: 0;
-    right: 0;
-    /* padding */
-    padding: 15px;
-    margin: 15px;
-    /* background */
-    background: rgba(255, 255, 255, 0.479);
-    /* border */
-    border-radius: 10px;
-    // drop shadow
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    backdrop-filter: blur(4px);
-    -webkit-backdrop-filter: blur(4px);
-  }
+.debug-view {
+  /* right bottom fixed */
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  /* padding */
+  padding: 15px;
+  margin: 15px;
+  /* background */
+  background: rgba(255, 255, 255, 0.479);
+  /* border */
+  border-radius: 10px;
+  // drop shadow
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+}
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,7 +12,8 @@
       :style="{
         overflow: 'overlay',
         background: 'transparent',
-      }">
+      }"
+    >
       <a-sub-menu key="sub1">
         <template #title>
           <a-flex align="center" gap="5">
@@ -35,129 +36,132 @@
       </a-sub-menu>
     </a-menu>
     <div class="user-card" @click="showLoginModal">
-      <div class="user-details">
-        <h3>{{user.UserData.姓名}}</h3>
-        <p>{{user.UserData.系所}}</p>
+      <div class="user-details" v-if="userStore.user.isLoggedIn">
+        <h3>{{ user.UserData.姓名 }}</h3>
+        <p>{{ user.UserData.系所 }}</p>
+      </div>
+      <div v-else>
+        登入
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import { router } from "../router";
-  import { useRoute } from "vue-router";
-  import { useGlobalStateStore } from "../stores/GlobalState";
-  import { useUserStore } from "../stores/user";
-  import { storeToRefs } from "pinia";
+import { router } from "../router";
+import { useRoute } from "vue-router";
+import { useGlobalStateStore } from "../stores/GlobalState";
+import { useUserStore } from "../stores/user";
+import { storeToRefs } from "pinia";
 
-  export default {
-    components: {},
-    setup() {
-      const userStore = useUserStore();
-      const GlobalStateStore = useGlobalStateStore();
-      const { user } = storeToRefs(userStore);
-      return { userStore, GlobalStateStore ,user};
-    },
-    data() {
-      return {
-        Sider: {
-          openKeys: ["sub1", "sub2"],
-          collapsed: false,
+export default {
+  components: {},
+  setup() {
+    const userStore = useUserStore();
+    const GlobalStateStore = useGlobalStateStore();
+    const { user } = storeToRefs(userStore);
+    return { userStore, GlobalStateStore, user };
+  },
+  data() {
+    return {
+      Sider: {
+        openKeys: ["sub1", "sub2"],
+        collapsed: false,
+      },
+      currentPage: ["/"],
+      menuItems: [
+        {
+          text: "首頁",
+          icon: "home-icon",
+          isOpen: false,
         },
-        currentPage: ["/"],
-        menuItems: [
-          {
-            text: "首頁",
-            icon: "home-icon",
-            isOpen: false,
-          },
-          {
-            text: "課程系統",
-            icon: "education-icon",
-            isOpen: false,
-            children: ["課程查詢", "選課系統"],
-          },
-          {
-            text: "個人資訊",
-            icon: "user-icon",
-            isOpen: false,
-            children: ["成績查詢", "學分分析"],
-          },
-        ],
-      };
+        {
+          text: "課程系統",
+          icon: "education-icon",
+          isOpen: false,
+          children: ["課程查詢", "選課系統"],
+        },
+        {
+          text: "個人資訊",
+          icon: "user-icon",
+          isOpen: false,
+          children: ["成績查詢", "學分分析"],
+        },
+      ],
+    };
+  },
+  computed: {
+    currentRoute() {
+      return useRoute();
     },
-    computed: {
-      currentRoute() {
-        return useRoute();
-      },
+  },
+  methods: {
+    changePage({ key }: { key: string }) {
+      console.log(`Change page to ${key}`);
+      router.push(key);
     },
-    methods: {
-      changePage({ key }: { key: string }) {
-        console.log(`Change page to ${key}`);
-        router.push(key);
-      },
-      showLoginModal() {
-        this.GlobalStateStore.setLoginModal(true);
-      },
-      GoHome() {
-        router.push("/");
-      },
+    showLoginModal() {
+      this.GlobalStateStore.setLoginModal(true);
     },
-    watch: {
-      "currentRoute.fullPath"(val: string) {
-        this.currentPage = [val];
-      },
+    GoHome() {
+      router.push("/");
     },
-  };
+  },
+  watch: {
+    "currentRoute.fullPath"(val: string) {
+      this.currentPage = [val];
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  .sidebar {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-  }
-  .user-card {
-    margin-top: auto;
-    margin-bottom: 12px;
-    margin-left: 12px;
-    margin-right: 12px;
-    padding: 20px;
-    display: flex;
-    align-items: center;
-    background-color: var(--highlight-color);
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  }
+.sidebar {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.user-card {
+  margin-top: auto;
+  margin-bottom: 12px;
+  margin-left: 12px;
+  margin-right: 12px;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  background-color: var(--highlight-color);
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
 
-  .user-image {
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    margin-right: 10px;
-    background-color: #eaeaea;
-  }
+.user-image {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin-right: 10px;
+  background-color: #eaeaea;
+}
 
-  .user-details {
-    h3 {
-      margin: 0;
-      color: var(--base-color);
-    }
-    p {
-      margin: 0;
-      color: var(--base-color-0);
-    }
+.user-details {
+  h3 {
+    margin: 0;
+    color: var(--base-color);
   }
+  p {
+    margin: 0;
+    color: var(--base-color-0);
+  }
+}
 
-  .title {
-    font-size: 1.5rem;
-  }
+.title {
+  font-size: 1.5rem;
+}
 </style>
 
 <style lang="scss">
-  .ant-menu-light {
-    .ant-menu-submenu-selected > .ant-menu-submenu-title {
-      color: var(--base-color);
-    }
+.ant-menu-light {
+  .ant-menu-submenu-selected > .ant-menu-submenu-title {
+    color: var(--base-color);
   }
+}
 </style>
